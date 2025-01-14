0a844dec5e0c49a1d489f1d3134537fdcafa5378
Cervator <cervator@gmail.com>
2025-01-14 17:18:54 -0500

    Add more Wekan WIP from the past

--- wekan/features/steps/wekan.py
+++ wekan/features/steps/wekan.py
@@ -1,25 +1,29 @@
 import logging
+import os
 from behave import *
 from kubernetes import client, config
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+# Get the namespace from the environment variables
+namespace = os.getenv('NAMESPACE', 'default')
+
 @given('the WeKan Helm chart is deployed')
 def step_impl(context):
     config.load_kube_config()  # Load Kubernetes configuration
     v1 = client.AppsV1Api()
     try:
-        deployment = v1.read_namespaced_deployment("wekan", "wekan")
+        deployment = v1.read_namespaced_deployment("wekan", namespace)
         assert deployment is not None, "Deployment not found"
-        logging.info(f"Deployment 'wekan' found in 'wekan' namespace")
+        logging.info(f"Deployment 'wekan' found in '{namespace}' namespace")
     except client.ApiException as e:
         assert False, f"Exception when calling AppsV1Api->read_namespaced_deployment: {e}"
 
 @then('the WeKan pods should be running')
 def step_impl(context):
     v1 = client.CoreV1Api()
-    pods = v1.list_namespaced_pod("wekan")
+    pods = v1.list_namespaced_pod(namespace)
     for pod in pods.items:
         assert pod.status.phase == "Running", f"Pod {pod.metadata.name} is not running"
-        logging.info(f"Pod {pod.metadata.name} is running in 'wekan' namespace")
+        logging.info(f"Pod {pod.metadata.name} is running in '{namespace}' namespace")
